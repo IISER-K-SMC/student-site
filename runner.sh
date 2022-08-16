7d9823592ba5cb7bd22ec6a20b44d1c95751cfe3
Harshith <pystardust@protonmail.com>
2022-08-17 00:50:42 +0530

    removed runner

--- runner.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-echo "Starting Dev Setup"
-
-tmux new-window ". venv/bin/activate;
-				 . loadenv.sh;
-				 cd backend; uvicorn --reload --reload-dir server/ server:app;"
-tmux split-window -dh -c "frontend/" "npm run dev"
-tmux rename-window "Runners"
